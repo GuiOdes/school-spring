79e33437bfce051399045aca2448b329d3763873
Guilherme Monteiro <guilherme.montnezes@gmail.com>
2022-03-11 13:04:58 -0300

    Student exceptions

--- src/main/java/com/school/management/system/ExceptionHandler/Properties.java
+++ src/main/java/com/school/management/system/ExceptionHandler/Properties.java
@@ -1,16 +1,21 @@
 package com.school.management.system.ExceptionHandler;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Properties {
 
     private String status;
+    private String title;
     private List<Field> fieldList;
 
     @AllArgsConstructor
